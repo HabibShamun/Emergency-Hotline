4e40a961d08c72a8c9a7113184b058347cf5ba7e
Habib Shamun <habibsalehinshamun@gmail.com>
2025-08-29 02:58:13 +0600

    added js functionality to heart

---
+++ index.js
@@ -0,0 +1,9 @@
+function increaseHeart() {
+    let increase=parseInt(document.getElementById("increaseHeart").innerText);
+    increase++;
+    document.getElementById("increaseHeart").innerText=increase;
+}
+
+function call() {
+    
+}
