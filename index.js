2b01b046cb615dfcb08da82beab39dc0263b2340
Habib Shamun <habibsalehinshamun@gmail.com>
2025-08-29 04:12:25 +0600

    added js functionality to to call, and fixed problems related to cards

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ function increaseHeart() {
     document.getElementById("increaseHeart").innerText=increase;
 }
 
-function call() {
-    
+function call(text, num) {
+    let coins = parseInt(document.getElementById("coins").innerText);
+    if (coins >= 20) {
+        alert("📞calling " + text + " " + num);
+        coins -= 20;
+        document.getElementById("coins").innerText = coins;
+        document.getElementById("history-add").innerHTML += `
+            <div class="text-sm text-gray-700 my-1">
+                <p>📞 ${text}</p>
+                <p>📱 ${num}</p>
+            </div>
+        `;
+    } else {
+        alert("❌You have insufficient coins! You need at least 20");
+    }
+}
+
+
 }
