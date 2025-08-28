219f327dfd35b9b265ad336234d3c1caa17fb098
Habib Shamun <habibsalehinshamun@gmail.com>
2025-08-29 05:20:02 +0600

    finished with everything

--- index.js
+++ index.js
@@ -4,22 +4,44 @@ function increaseHeart() {
     document.getElementById("increaseHeart").innerText=increase;
 }
 
-function call(text, num) {
-    let coins = parseInt(document.getElementById("coins").innerText);
-    if (coins >= 20) {
-        alert("📞calling " + text + " " + num);
-        coins -= 20;
-        document.getElementById("coins").innerText = coins;
+function call(text,num) {
+    let coins=parseInt(document.getElementById("coins").innerText);
+    if(coins>=20) {
+        alert("📞calling "+text+" "+num);
+        coins-=20;
+        document.getElementById("coins").innerText=coins;
+        let now = new Date();
+        let time= now.toLocaleTimeString();
+        document.getElementById("history-add").className=""
         document.getElementById("history-add").innerHTML += `
-            <div class="text-sm text-gray-700 my-1">
-                <p>📞 ${text}</p>
-                <p>📱 ${num}</p>
-            </div>
-        `;
+    <div class="bg-[#F5FFF6] py-[10px] px-[20px] mb-[30px] flex justify-between items-center rounded-xl">
+        <div >
+        <p class="font-semibold">${text}</p>
+        <p class="font-regular">${num}</p>
+        </div>
+        <p class="font-regular">${time};
+        </p>
+    </div>
+`;
     } else {
-        alert("❌You have insufficient coins! You need at least 20");
+        alert("❌You have insufficient coins! you need atleast 20");
     }
+
 }
 
 
+function clearHistory() {
+    document.getElementById("history-add").innerHTML="";
+}
+
+function copyText() {
+   let copied= document.getElementById("hotLineNumber").innerText;
+   let times=parseInt(document.getElementById("timesCopied").innerText);
+   navigator.clipboard.writeText(copied).then(function(){
+    alert("Copied: "+copied);
+    times++;
+    document.getElementById("timesCopied").innerText=times;
+   }).catch(err,function(){
+    console.error("Failed to copy",err);
+   })
 }
